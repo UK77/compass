240f6a18648d862836be84f4b102dae96b929800
Kazuki Uwai <uk.wolf0707@gmail.com>
2020-06-29 15:33:28 +0900

    Initial Commit

---
+++ compass/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  compass
+//
+//  Created by 上井一起 on 2020/06/29.
+//  Copyright © 2020 上井一起. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
