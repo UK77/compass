4fc5b7dd8cabd96ee3999536397c3e6121abe31d
Kazuki Uwai <uk.wolf0707@gmail.com>
2020-06-30 23:24:10 +0900

    updateLocationをRequestしない

--- compass/MapScreen.swift
+++ compass/MapScreen.swift
@@ -44,7 +44,6 @@ class MapScreen: UIViewController {
 		return (start, goalLatitude, goalLongitude)
 	}
 	
-	
 	func centerViewOnUserLocation(){
 		if let location = locationManager.location?.coordinate{
 			let region = MKCoordinateRegion.init(center: location, latitudinalMeters: regionInMeters, longitudinalMeters: regionInMeters)
@@ -95,12 +94,6 @@ class MapScreen: UIViewController {
 }
 
 extension MapScreen: CLLocationManagerDelegate {
-	func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-		guard let location = locations.last else { return }
-		let center = CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
-		let region = MKCoordinateRegion.init(center: center, latitudinalMeters: regionInMeters, longitudinalMeters: regionInMeters)
-		mapView.setRegion(region, animated: true)
-	}
 	func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
 		checkLocationAuthorization()
 	}
