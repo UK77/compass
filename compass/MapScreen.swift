d2ff4670e166ba4ccc6fd82fd757178868027b67
Kazuki Uwai <uk.wolf0707@gmail.com>
2020-07-01 10:30:41 +0900

    LocationAuthorizationに関して、使用中のみの許可を得られていない場合にアラートを表示して設定画面へ遷移させるコードを追加

--- compass/MapScreen.swift
+++ compass/MapScreen.swift
@@ -75,15 +75,43 @@ class MapScreen: UIViewController {
 			break
 		case .denied:
 			// show alert instructing them to turn on permissions
+			alertLocationAuthorization()
 			break
 		case .notDetermined:
 			locationManager.requestWhenInUseAuthorization()
 		case .restricted:
 			// show alert letting them know what's up
+			alertLocationAuthorization()
 			break
 		case .authorizedAlways:
+			mapView.showsUserLocation = true
+			centerViewOnUserLocation()
+			locationManager.startUpdatingLocation()
+			locationManager.startUpdatingHeading()
 			break
 		}
+	}
+	
+	func alertLocationAuthorization(){
+		let alertController = UIAlertController (title: "位置情報の共有が\n許可されていません", message: "このAppを使用するには位置情報を\n\"このAppの使用中のみ許可\"\nに設定してください", preferredStyle: .alert)
+
+		let settingsAction = UIAlertAction(title: "設定へ", style: .default) { (_) -> Void in
+
+			guard let settingsUrl = URL(string: UIApplication.openSettingsURLString) else {
+				return
+			}
+
+			if UIApplication.shared.canOpenURL(settingsUrl) {
+				UIApplication.shared.open(settingsUrl, completionHandler: { (success) in
+					print("Settings opened: \(success)") // Prints true
+				})
+			}
+		}
+		alertController.addAction(settingsAction)
+		let cancelAction = UIAlertAction(title: "キャンセル", style: .default, handler: nil)
+		alertController.addAction(cancelAction)
+
+		present(alertController, animated: true, completion: nil)
 	}
 	
 	func getCenterLocation(for mapView: MKMapView) -> CLLocation{
